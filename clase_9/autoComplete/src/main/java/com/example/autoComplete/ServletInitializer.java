4c2b675a6edafcb01a7b15fc04c0ec3e23693bd0
Mpablom <pablomoya211@gmail.com>
2023-07-22 22:59:37 -0300

    cambio el lugar de la carpeta raiz

---
+++ clase_9/autoComplete/src/main/java/com/example/autoComplete/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.autoComplete;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AutoCompleteApplication.class);
+	}
+
+}
